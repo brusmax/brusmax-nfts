8dcd356cf487faa08b1c38f3c4be97b4f155f1a8
brusmax <enrique.webgenius@gmail.com>
2022-09-01 23:06:31 -0500

    Contract ERC721 started + some config added

---
+++ contracts/BrusmaxPunks.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Base64.sol";
+
+
+contract BrusmaxPunks is ERC721, ERC721Enumerable {
+  using Counters for Counters.Counter;
+  Counters.Counter private _idCounter;
+  uint256 public maxSupply;
+
+  constructor(uint256 _maxSupply) ERC721("BrusmaxPunks", "MXPKS"){
+    maxSupply = _maxSupply;
+
+  }
+
+  function mint() public {
+    uint256 current = _idCounter.current();
+    require(current < maxSupply, "No Brusmax Ponks left :(");
+    _safeMint(msg.sender, current);
+    _idCounter.increment();
+  }
+
+  function tokenURI(uint256 tokenId) 
+  public 
+  view 
+  override
+  returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721 metadata: URI query for nonexistent token"
+      );
+    string memory jsonURI = Base64.encode(
+      abi.encodePacked(
+        '{ "name": "BrusmaxPunks #',
+        tokenId,
+        '", "description": "Brusmax Punks are randomized Avatars stored on chain to teach DApp development on Platzi",',
+        '"image": "// TODO: Calculate image URL"',
+        '}'
+      ));
+      return string(abi.encodePacked("data:application/json;base64,", jsonURI));
+  }
+
+   // Override required
+   function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+}
